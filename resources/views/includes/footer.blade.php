3557802ac3fea5f7c87645d7252cad0fec41ea58
Nazrin S. Tingstrom <nazrin.tingstrom@cornell.edu>
2019-04-22 13:59:32 -0400

    Adding emergency banner to the footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,4 +3,5 @@
 	<div class="container text-center">
 		<span>&#169;{{date("Y")}} Cornell University, {{env('DISPLAY_NAME')}}. All Rights Reserved.</span>
 	</div>
+	<script src="https://embanner.univcomm.cornell.edu/OWC-emergency-banner.js" type="text/javascript"></script>
 </footer>
