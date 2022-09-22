9c8ab4294a878b7721764a5277901443032e9d28
Nazrin Tingstrom <nst37@cornell.edu>
2022-09-21 23:48:02 -0400

    Footer change

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,4 +3,13 @@
 	<div class="container text-center">
 		<span>&#169;{{date("Y")}} Cornell University, {{env('DISPLAY_NAME')}}. All Rights Reserved.</span>
 	</div>
+    <br />
+    <div class="container two-col text-center">
+        <div>
+            <span><a href="https://privacy.cornell.edu/">University Privacy</a></span>
+        </div>
+        <div>
+            <span><a href="mailto:web-accessibility@cornell.edu">Web Accessibility Assistance</a></span>
+        </div>
+    </div> 	
 </footer>
