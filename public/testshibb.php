23ca7f107df07a4f69a662fc6095098f9ea136c4
Nazrin Tingstrom <nst37@cornell.edu>
2026-01-27 17:51:21 -0500

    resolved merge

---
+++ public/testshibb.php
@@ -0,0 +1,13 @@
+  <?php
+
+                 echo "<h3> PHP List All Server Variables</h3>";
+                 foreach ($_SERVER as $key=>$val)
+                 echo $key." ".$val."<br/>";
+
+                 // Display PHP INFO
+                 echo phpinfo()."<br />";
+
+                ?>
+
+
+
